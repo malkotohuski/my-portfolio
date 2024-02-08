4d4fd75e312de224fe87a5392e60a58864637704
Daniel Dimitrov <malkotohuski@gmail.com>
2024-02-08 14:39:11 +0200

    text-color-abd  backdrop-filter

--- src/Pages/Header/Category/contacts.js
+++ src/Pages/Header/Category/contacts.js
@@ -15,22 +15,22 @@ const ContactPage = () => {
     return (
         <div className="contact-container">
             <div className="contact-header">
-                <h1 style={{ color: 'black' }}>Contact Us</h1>
+                <h1 style={{ color: 'white' }}>Contact Us</h1>
                 <p>Please fill out the form below to get in touch with us.</p>
             </div>
             <form onSubmit={handleSubmit}>
                 <div className="form-row">
-                    <label>
+                    <label className='labelsNames'>
                         Name:
                         <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
                     </label>
-                    <label>
+                    <label className='labelsNames'>
                         Email:
                         <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
                     </label>
                 </div>
                 <div className='massage-row'>
-                    <label>
+                    <label className='labelsNames'>
                         Message:
                         <textarea value={message} onChange={(event) => setMessage(event.target.value)} />
                     </label>
